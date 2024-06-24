6a15a6bf304579895268b600174a5be49c11f6b9
Samyam Shrestha <82583939+SamyamCodes@users.noreply.github.com>
2024-06-24 18:03:50 +0545

    Update day01.py

--- Day_01/day01.py
+++ Day_01/day01.py
@@ -1,7 +1,2 @@
-print("Hello World")
-name = input(print("Your name is ?"))
-print(name)
-# This is a comment in python programming
-
-print(1+1)
-print("SAmyam")
+print("Hello Samyam, Welcome to PYthon programming")
+print(5+5)
