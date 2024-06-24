f71a5bd46ccc01131dde14a0691afb1e32cfbecf
Samyam Shrestha <82583939+SamyamCodes@users.noreply.github.com>
2024-06-24 18:55:35 +0545

    Update day_02.py

--- Day_02/day_02.py
+++ Day_02/day_02.py
@@ -1,5 +1,23 @@
 # This is Day_02
 # Variables
+ 
+a = complex(5,2)
+b = True
+c = "Samyam"
+d =None
+print("The type of a is" , type(a))
+print("The type of b is" , type(b))
+print("The type of c is" , type(c))
+print("The type of d is" , type(d))
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
 
 
 
