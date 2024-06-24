7cd70b78b0baedaec9e3d1d37b5504f065d9bdb7
Samyam Shrestha <82583939+SamyamCodes@users.noreply.github.com>
2024-06-24 18:41:29 +0545

    Learned about Print
    Print Statements

--- Day_01/day01.py
+++ Day_01/day01.py
@@ -1,2 +1,17 @@
-print("Hello Samyam, Welcome to PYthon programming")
-print(5+5)
+'''
+This is Day_01 Welcome to the learning journey.
+Author : Samyam Shrestha
+Course: 100 Days of Code
+'''
+
+print("Hello Samyam, Welcome to PYthon programming", 7+7)
+print("hey I am a good boy. \nand this boy is also good.")  # \n for a new line
+print(5)
+print("Bye")
+
+# Escape Sequence Character
+
+print("Hey Samyam. \"This is good.\"")
+
+print("Hey", 6, 8, sep="&", end="009\n", ) 
+print("Okay")
--- Day_02/day_02.py
+++ Day_02/day_02.py
@@ -1,2 +1,2 @@
-# This is day 02
-print("Samyam")
+
+
