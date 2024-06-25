32024003f6299eb815a9c09c649500589fc11606
Samyam Shrestha <82583939+SamyamCodes@users.noreply.github.com>
2024-06-25 20:46:43 +0545

    Update input.py
    Added the input operation with type casting

--- Day_04/input.py
+++ Day_04/input.py
@@ -1,2 +1,10 @@
 # Using input function
 
+a = input("Enter your name: ")
+print("Your name is: ", a)
+
+x = int(input("Enter the first number"))
+y = int(input("Enter the second number"))
+
+print("The sum of the number is: ", x+y)
+
