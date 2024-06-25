2dd5444507962fdbf8418ca1ae8b3b97c84567e9
Samyam Shrestha <82583939+SamyamCodes@users.noreply.github.com>
2024-06-25 20:57:21 +0545

    Update strings.py

--- Day_04/strings.py
+++ Day_04/strings.py
@@ -1,1 +1,28 @@
 # Learning Strings in Python
+name = "Samyam"
+caste = "Shrestha"
+
+print("Hello, " + name ," " + caste)
+
+name = "Samyam"
+friend = "SS"
+anotherFriend = 'Lov'
+apple = '''He said, 
+Hi Sam
+hey I am good      
+"I want to eat an apple'''
+ 
+# ''' Anything betwen this is a multi line comment.  '''
+
+print("Hello, " + name)
+print(apple) 
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5]) 
+print("Lets use a for loop\n")
+
+for character in apple:
+    print(character)
