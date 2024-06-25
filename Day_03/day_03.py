5e9be4294989cedf0f654671400f967dcb0a8f8e
Samyam Shrestha <82583939+SamyamCodes@users.noreply.github.com>
2024-06-25 20:26:31 +0545

    Create day_03.py

---
+++ Day_03/day_03.py
@@ -0,0 +1,17 @@
+# This is day 03
+# Calculator using python
+
+# print(5**3) Thuis is exponential, equal to 5 cube.
+
+a = 24
+b = 12
+
+add = a+b
+mul = a*b
+div = int(a/b)
+sub = a-b
+
+print(f"The addition of{a} and {b} is : {add} ")
+print(f"The multiplication of{a} and {b} is : {mul} ")
+print(f"The division of{a} and {b} is : {div} ")
+print(f"The subtraction of{a} and {b} is : {sub} ")
