18715ae09416aa21be820ec091c33e23d6a22db0
Samyam Shrestha <82583939+SamyamCodes@users.noreply.github.com>
2024-06-28 19:42:55 +0545

    Update string.py

--- Day_05/string.py
+++ Day_05/string.py
@@ -1,2 +1,25 @@
 # This is day 5 of python programming
 
+# Topic: Strings in Python.
+
+name = "Samyam"
+friend = "Shrestha"
+anotherfriend = 'Newari'
+
+apple = '''He said "I want to eat an apple'
+New line is printed
+this is fun
+all the things are in multipli line within (''' ''') this 
+triple single quote.
+
+'''
+print(name[0])
+print("Hello," + name)
+print(apple)
+
+print("Let's use a for loop\n")
+for character in name:
+    print(character)
+
+for character in apple:
+    print(character)
