072b4113523065e7ba46ee58fc6dd7de76c8cb41
Samyam Shrestha <82583939+SamyamCodes@users.noreply.github.com>
2024-06-29 19:45:26 +0545

    Create matchcase.py

---
+++ Day_07/matchcase.py
@@ -0,0 +1,14 @@
+# This is Day_07
+
+# Title: Match Case in Python
+
+x = int(input("Enter the value of x: "))
+
+match x:
+    case 0:
+        print("x is zero")
+    case 4:
+        print("x is 4.")
+    case _:
+        print(x)
+
