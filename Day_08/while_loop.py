40112d846ff0b60b271d33292dd4b13eb99f6e00
Samyam Shrestha <82583939+SamyamCodes@users.noreply.github.com>
2024-06-30 16:23:31 +0545

    Whileloop

---
+++ Day_08/while_loop.py
@@ -0,0 +1,13 @@
+i = 0
+while (i <5):
+    i = i+1
+    print(i)
+   
+
+# Decremental loop
+count = 5
+while (count > 0):
+    print(count)
+    count = count -1
+else:
+    print("I am inside else.")
