3b63d88d53df2902b72bfe8c501267b2431e5717
Samyam Shrestha <82583939+SamyamCodes@users.noreply.github.com>
2024-06-30 16:10:45 +0545

    Create loop.py

---
+++ Day_08/loop.py
@@ -0,0 +1,20 @@
+# This is Day_08
+# Title: For Loop
+
+# name = "Samyam"
+# for i in name:
+#     print(i)
+
+# colors = ["Red", "Yellow", "Blue", "Green", "Black"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+# Range
+# for k in range (5):  # goes from 0 to 4
+    # print(k)
+
+for i in range(1, 12, 2): # Here, 2 is the step.
+    print(i)
+
