86e78bd154c5de84410a06c7d3dd603cd3b2708a
Samyam Shrestha <82583939+SamyamCodes@users.noreply.github.com>
2024-07-06 22:26:30 +0545

    Update myblink.py

--- Day_01_RaspBerryPi/myblink.py
+++ Day_01_RaspBerryPi/myblink.py
@@ -1,11 +1,15 @@
 import RPi.GPIO as GPIO
-import time
+import time 
+cont = 'Y'
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
-numBlink = int(input("How many Blinks DO you  wish for: "))
-for i in range (0, numBlink):
-        GPIO.output(11, 1)
-        time.sleep(1)
-        GPIO.output(11, 0)
-        time.sleep(1)
+while cont =='Y':
+        numBlink = int(input("How many Blinks DO you  wish for: "))
+        for i in range (0, numBlink):
+                GPIO.output(11, 1)
+                time.sleep(1)
+                GPIO.output(11, 0)
+                time.sleep(1)
+                cont = input("Do You Want to Continue: (Y for Yes) ")
 GPIO.cleanup()
+
