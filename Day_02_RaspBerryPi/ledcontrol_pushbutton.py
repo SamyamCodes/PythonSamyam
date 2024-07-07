4610938ed91d685bde00fc6d944c51254b911364
Samyam Shrestha <82583939+SamyamCodes@users.noreply.github.com>
2024-07-07 18:48:38 +0545

    Update ledcontrol_pushbutton.py

--- Day_02_RaspBerryPi/ledcontrol_pushbutton.py
+++ Day_02_RaspBerryPi/ledcontrol_pushbutton.py
@@ -11,11 +11,37 @@ GPIO.setup(led_pin, GPIO.OUT)
 try:
     while True:
         readVal = GPIO.input(inputPin)
-        if readVal ==1:
+        if readVal ==1:         #This code is for the circuit that is connnecte woth the pin no 1 of 3.3V.
             displayVal = GPIO.output(led_pin, 0)
         if readVal == 0:
             displayVal = GPIO.output(led_pin, 1)        
 except KeyboardInterrupt():
     GPIO.cleanup()
     print("That's all Guys.")
+
+# Now, let's write the code for the circuit that is dircetly connected from the push button and the led +ve is also given to GPIO pin 38
+import RPi.GPIO as GPIO
+from time import sleep
+delay = .1
+led = 38
+inputpin = 40
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(inputpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # This is done for pull up / down resistor inside the raspberrypi by itself.
+
+try:
+    while True:
+        readVal = GPIO.input(inputpin, GPIO.IN)
+        print(readVal)
+        if readVal == 1:
+            GPIO.output(led, 0)
+        if readVal == 0:
+            GPIO.output(led, 1)
+        sleep(delay)
+except KeyboardInterrupt():  # Interrupt is obtained by Ctrl + C
+    GPIO.cleanup()
+    print("GPIO ready to Go.")
+
 
