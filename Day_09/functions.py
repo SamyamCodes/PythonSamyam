17ba84e2fb1754191c8e212910ad1a4f15eeea27
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-08-01 16:47:14 +0545

    Update functions.py

--- Day_09/functions.py
+++ Day_09/functions.py
@@ -1,3 +1,22 @@
 # This is Day_09
 # Title: function
-print ("Samyam")
+
+def isGreater(a, b):
+    if (a>b):
+        print("First number is greater.")
+    else:
+        print("Second number is greater.")
+
+def calculateGmean(a, b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+a = 9
+b = 10
+
+calculateGmean(a, b)
+isGreater (a, b)
+
+def isless(a, b):
+    pass # This means I want to write the function code later.
+
