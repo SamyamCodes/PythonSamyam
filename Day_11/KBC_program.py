2297021395b4d7126158fe38bc1e132b3f49f25e
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-08-04 20:34:09 +0545

    Create KBC_program.py

---
+++ Day_11/KBC_program.py
@@ -0,0 +1,33 @@
+# Create a program capable of displaying questions to the user like KBC (Kaun Banega Crorepati?)
+# Use data type to store the questions and their correct answers.
+# Display the final amount the user is taking home after palying the game.
+
+# total_questions = 10
+# user_obtained_points = 0
+# print("\t\t\t\tWelcome to Kaun Banega Crorepati.\t\t\t\t")
+# print("\t\t\t\tCarefully read the rules. \n \t\t\t\t1) There are 10 sets of questions divided into three levels. \n \t\t\t\t2) Each correct answer in level 1 results 100 points, for level 2, 200 points and for level 3, 300 points. ")
+
+# def level_1 ():
+#     print("The questioins levels are general.")
+# def level_2():
+#     print("The question levels are sligtly tough.")
+# def level_3():
+#     print("The questions level are hifhly difficult. Think Carefully before giving the answer.")
+
+# This is the basic simplest idea for KBC. It is baby level code
+# question_1 = ["Which is living object?"]
+# option_1 =  ("1) Cat", "2) Brick", "3) Stone", "4) Road")
+# print(question_1)
+# print(option_1)
+# user_answer = int(input('Enter the correct option:'))
+# correct_answer = 1
+# if(user_answer == correct_answer):
+#     print("Hooray! You have won 100 points.")
+#     user_obtained_points +=100
+# else:
+#     print("Sorry, the answer is wrong. You are disqualified.")
+
+questions = [""]
+
+
+
