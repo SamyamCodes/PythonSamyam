e7e8e5c881a2d7444468c46a297cf1484d91f553
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-08-13 20:25:38 +0545

    Calculated the weekly and daily expenditure

--- HelsinkiPython/Part1/singleline.py
+++ HelsinkiPython/Part1/singleline.py
@@ -26,3 +26,15 @@ print(" - ", end="")
 print(4, end="")
 print(" = ", end="")
 print(5 + 8 - 4)
+
+# Please write a program which asks the user for a number. The program then prints out the number multiplied by five.
+
+# The program should function as follows:
+
+# Sample output
+# Please type in a number: 3
+# 3 times 5 is 15
+
+# Write your solution here
+number = int(input("Please type in a number:"))
+print(f"{number} times 5 is {number*5}")
