ea455f2c90e6417e424fb6f4bda3d003309b1f04
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-08-13 18:02:21 +0545

    Update part1.py

--- HelsinkiPython/part1.py
+++ HelsinkiPython/part1.py
@@ -33,3 +33,18 @@ part1 = input("The 1st part: ")
 part2 = input("The 2nd part: ")
 part3 = input("The 3rd part: ")
 print(part1+"-" + part2+"-" + part3+"!")
+
+# Programming exercise:
+# Story
+# Please write a program which prints out the following story. The user gives a name and a year, which should be inserted into the printout.
+
+# Sample output
+# Please type in a name: Mary
+# Please type in a year: 1572
+
+# Mary is a valiant knight, born in the year 1572. One morning Mary woke up to an awful racket: a dragon was approaching the village. Only Mary could save the village's residents.
+
+# Write your solution here
+name = input("Please type in a name:")
+year = input("Please type in a year.")
+print(name+" is a valiant knight, born in the year "+ year+ ". One morning "+name+" woke up to an awful racket: a dragon was approaching the village. Only "+name+" could save the village's residents.")
