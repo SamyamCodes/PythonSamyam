e7039d9dd848d1351a1a8e252ecceceadc61d46c
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-08-15 20:16:55 +0545

    Update programmingtechnology.py

--- HelsinkiPython/Part2/programmingtechnology.py
+++ HelsinkiPython/Part2/programmingtechnology.py
@@ -8,3 +8,15 @@ if (number>100):
 print( f"{number} must be my lucky number!")
 print("Have a nice day!")
 
+# Programming exercise:
+# Number of characters
+
+# The function len can be used to find out the length of a string, among other things. 
+# The function returns the number of characters in a string.
+# Write your solution here
+word = input("Please type in a word:")
+if(len(word)>1):
+    print(f"There are {len(word)} letters in the word {word}")
+    print("Thank you!")
+else:
+    print("Thank you!")
