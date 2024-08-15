ef2ee51855fcf5471da4be8712ea79b052a7f589
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-08-15 20:23:21 +0545

    Update programmingtechnology.py

--- HelsinkiPython/Part2/programmingtechnology.py
+++ HelsinkiPython/Part2/programmingtechnology.py
@@ -20,3 +20,19 @@ if(len(word)>1):
     print("Thank you!")
 else:
     print("Thank you!")
+
+# Please write a program which asks the user for a floating point number and then prints out the integer part and the decimal part separately. Use the Python int function.
+
+# You can assume the number given by the user is always greater than zero.
+
+# An example of expected behaviour:
+
+# Sample output
+# Please type in a number: 1.34
+# Integer part: 1
+# Decimal part: 0.34
+
+# Write your solution here
+number = float(input("Please type in a number:"))
+print(f"Integer part: {int(number)}")
+print(f"Decimal part: {number-int(number)}")
