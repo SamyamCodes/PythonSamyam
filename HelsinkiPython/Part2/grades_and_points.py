dd6706b7cb53c750c54939a399e4be67cf488f51
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-08-16 17:52:20 +0545

    Update grades_and_points.py

--- HelsinkiPython/Part2/grades_and_points.py
+++ HelsinkiPython/Part2/grades_and_points.py
@@ -28,3 +28,23 @@ How many points [0-100]: -3
 Grade: impossible!
 '''
 # Write your solution here
+# Write your solution here
+points = int(input("How many points [0-100]:"))
+if (points < 0 or points > 100):
+    print("Grade: impossible!")
+elif(points >= 0 and points < 50):
+    print("Grade: fail")
+elif(points >= 50 and points < 60):
+    print("Grade: 1")
+elif(points >= 60 and points < 70):
+    print("Grade: 2")
+elif(points >= 70 and points <80):
+    print("Grade: 3")
+elif(points >= 80 and points <90):
+    print("Grade: 4")
+elif(points >= 90 and points <=100):
+    print("Grade: 5")
+
+
+
+
