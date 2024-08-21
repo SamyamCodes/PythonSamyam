746b7154d2ad774a79982d57f06c932fad7796f7
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-08-21 20:08:50 +0545

    name

---
+++ Day_18/main.py
@@ -0,0 +1,4 @@
+import samyam
+
+samyam.welcome()
+
---
+++ Day_18/samyam.py
@@ -0,0 +1,8 @@
+def welcome():
+    print("Welcome to the module developed by Samyam.")
+
+
+print(__name__)
+if __name__ == "__main__":
+    welcome()
+
