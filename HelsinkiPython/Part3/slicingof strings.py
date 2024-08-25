312d4fb9309d3dfaccd7d5d44d5e6fb94b249d4b
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-08-25 19:29:18 +0545

    Update slicingof strings.py

---
+++ HelsinkiPython/Part3/slicingof strings.py
@@ -0,0 +1,7 @@
+'''
+If you know the beginning and end indexes of the slice 
+you wish to extract, you can do so with the notation [a:b]. T
+his means the slice begins at the index a and ends at 
+the last character before index b - that is,
+ including the first, but excluding the last
+'''
