8de4ba5e01cd9a642a8ddcbb095a8dd20a062cf9
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-08-25 20:23:32 +0545

    Update doesit_contain_vowels.py

--- HelsinkiPython/Part3/doesit_contain_vowels.py
+++ HelsinkiPython/Part3/doesit_contain_vowels.py
@@ -19,3 +19,52 @@ e not found
 o not found
 '''
 
+# Write your solution here
+ask = input("Please type in a string:")
+s1 = "a"
+s2 = "e"
+s4 = "o"
+while True:
+    if s1 in ask and s2 in ask and s4 in ask:
+        print("a found")
+        print("e found")
+        print("o found")
+    
+    elif s1 in ask and s2 in ask:
+        print("a found")
+        print("e found")
+        print("o not found")
+    elif s1 in ask and s4 in ask:
+        print("a found")
+        print("e not found")
+        print("o found")
+
+    elif s2 in ask and s4 in ask:
+        print("a not found")
+        print("e found")
+        print("o found")  
+
+    elif s1 in ask:
+        print("a found")
+        print("e not found")
+        print("o not found")
+        
+    elif s2 in ask:
+        print("a not found")
+        print("e found")
+        print("o not found")
+      
+    elif s4 in ask:
+        print("a not found")
+        print("e not found")
+        print("o found")
+    else:
+        print("a not found")
+        print("e not found")
+        print("o not found")
+
+
+    break  
+
+
+        
