bf7ac6684bfdc46f8559eda6de61aac5d2c045f4
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-08-25 19:30:28 +0545

    Update slicingof strings.py

--- HelsinkiPython/Part3/slicingof strings.py
+++ HelsinkiPython/Part3/slicingof strings.py
@@ -5,3 +5,13 @@ his means the slice begins at the index a and ends at
 the last character before index b - that is,
  including the first, but excluding the last
 '''
+input_string = "presumptious"
+
+print(input_string[0:3])
+print(input_string[4:10])
+
+# if the beginning index is left out, it defaults to 0
+print(input_string[:3])
+
+# if the end index is left out, it defaults to the length of the string
+print(input_string[4:])
