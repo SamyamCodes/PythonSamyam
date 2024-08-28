d8c98f290e67e4be51ecbb43b6309601222dff26
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-08-28 06:54:02 +0545

    Create find_all_substrings.py

---
+++ HelsinkiPython/Part3/find_all_substrings.py
@@ -0,0 +1,38 @@
+'''
+Please make an extended version of the previous program, 
+which prints out all the substrings which are at least three characters long, 
+and which begin with the character specified by the user. 
+You may assume the input string is at least three characters long.
+
+Sample output
+Please type in a word: mammoth
+Please type in a character: m
+mam
+mmo
+mot
+
+Sample output
+Please type in a word: banana
+Please type in a character: n
+nan
+
+Hint the following example may give you some inspiration as to how this exercise could be tackled:
+
+word = input("Word: ")
+while True:
+    if len(word) == 0:
+        break
+    print(word)
+    word = word[2:]
+'''
+
+# Get input from the user
+word = input("Please type in a word: ")
+char = input("Please type in a character: ")
+
+
+for i in range(len(word) - 2):
+    
+    if word[i] == char:
+        substring = word[i:i + 3]
+        print(substring)
