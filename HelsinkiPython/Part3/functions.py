820c7510b6fbc27175b1f6ceb39f896f256cc930
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-08-30 21:27:23 +0545

    Create functions.py

---
+++ HelsinkiPython/Part3/functions.py
@@ -0,0 +1,28 @@
+'''
+Important: on this course the automatic tests that are run on the exercise files 
+require an empty main function. No commands should be left in the main function of 
+your solution. That is, any code that you yourself use for testing must be contained 
+in a specially defined if block:
+
+def greet():
+    print("Hi!")
+
+# Write your main function within a block like this:
+if __name__ == "__main__":
+    greet()
+'''
+
+# Write your solution here
+# You can test your function by calling it within the following block
+# if __name__ == "__main__":
+#   seven_brothers()
+def seven_brothers():
+    print("Aapo")
+    print("Eero")
+    print("Juhani")
+    print("Lauri")
+    print("Simeoni")
+    print("Timo")
+    print("Tuomas")
+if __name__ == "__main__":
+    seven_brothers()
