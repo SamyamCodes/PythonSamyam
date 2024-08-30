77d788fed33dd3632d958d5659bfa62c7c366712
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-08-30 22:42:51 +0545

    Create hello_visual_studio_code.py

---
+++ HelsinkiPython/Part4/hello_visual_studio_code.py
@@ -0,0 +1,9 @@
+while True:
+    editori = input("editori: ")
+    if editori.lower() == "visual studio code":
+        break
+    if editori == "word" or editori == "notepad":
+        print("awful")
+    else:
+        print("not good")
+print("an excellent choice!")
