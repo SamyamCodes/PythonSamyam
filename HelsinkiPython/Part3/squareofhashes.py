8e4fbd433271c057741f05c807bff99876323aa7
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-08-30 21:55:25 +0545

    Create squareofhashes.py

---
+++ HelsinkiPython/Part3/squareofhashes.py
@@ -0,0 +1,10 @@
+# Write your solution here
+def hash_square(number):
+    x =1
+    while x <= number:
+        print("#"*number)
+        x+=1
+
+# You can test your function by calling it within the following block
+if __name__ == "__main__":
+    hash_square(5)
