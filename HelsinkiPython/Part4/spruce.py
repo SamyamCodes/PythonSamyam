9e46c7dc6a498cc9e5962fac542989c55ad6a646
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-09-01 20:55:17 +0545

    c

---
+++ HelsinkiPython/Part4/spruce.py
@@ -0,0 +1,32 @@
+'''
+Please write a function named spruce, which takes one argument. 
+The function prints out the text a spruce!, and the a spruce tree, the size of which is specified by the
+argument.
+
+Calling spruce(3) should print out
+
+Sample output
+a spruce!
+  *
+ ***
+*****
+  *
+Calling spruce(5) should print out
+
+Sample output
+a spruce!
+    *
+   ***
+  *****
+ *******
+*********
+    *
+
+    
+NB: to the left of the spruce there should be exactly the right amount of whitespace. 
+If the shape of the spruce looks correct, but the left edge of the tree is not touching the left edge of 
+the text area in the terminal, the tests will not accept the solution.
+    
+'''
+
+
--- HelsinkiPython/Part4/traingle.py
+++ HelsinkiPython/Part4/traingle.py
@@ -7,13 +7,12 @@ def line(x, st):
         print(st[0]* x_int)
 
 def triangle(size):
-    # You should call function line here with proper parameters
     x = 1
     num = 1
     while x < (size +1):
         line(num, "#")
         num += 1
         x +=1
-# You can test your function by calling it within the following block
+
 if __name__ == "__main__":
     triangle(5)
