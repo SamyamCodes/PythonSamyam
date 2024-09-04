fab2a1075d15470597dbe875960dfb44b81ea7bd
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-09-04 22:44:45 +0545

    Create list_twice.py

---
+++ HelsinkiPython/Part1/list_twice.py
@@ -0,0 +1,47 @@
+'''
+Please write a program which asks the user to type in values and adds them to a list. 
+After each addition, the list is printed out in two different ways:
+
+    in the order the items were added
+    ordered from smallest to greatest
+
+The program exits when the user types in 0.
+
+An example of expected behaviour:
+
+Sample output
+New item: 3
+The list now: [3]
+The list in order: [3]
+New item: 1
+The list now: [3, 1]
+The list in order: [1, 3]
+New item: 9
+The list now: [3, 1, 9]
+The list in order: [1, 3, 9]
+New item: 5
+The list now: [3, 1, 9, 5]
+The list in order: [1, 3, 5, 9]
+New item: 0
+Bye!
+'''
+# Write your solution here
+
+my_list = []
+
+while True:
+   
+    item = int(input("New item: "))
+    
+    
+    if item == 0:
+        break
+    
+    my_list.append(item)
+    
+   
+    print(f"The list now: {my_list}")
+  
+    print(f"The list in order: {sorted(my_list)}")
+
+print("Bye!")
