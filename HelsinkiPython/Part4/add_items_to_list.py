d046f0ca4ba32945d13b4a217a1fea99129102cd
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-09-04 21:22:09 +0545

    append

---
+++ HelsinkiPython/Part4/add_items_to_list.py
@@ -0,0 +1,16 @@
+'''
+Please write a program which first asks the user for the number of items to be added. 
+Then the program should ask for the given number of values, one by one, 
+and add them to a list in the order they were typed in. 
+Finally, the list is printed out.
+
+An example of expected behaviour:
+
+Sample output
+How many items: 3
+Item 1: 10
+Item 2: 250
+Item 3: 34
+[10, 250, 34]
+'''
+
