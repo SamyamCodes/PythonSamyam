e22b81aee95bde1522c1357e8d0fab2bf4067a0f
Samyam Shrestha <samyamshrestha48@gmail.com>
2024-09-06 07:38:41 +0545

    Create everything_revrsed.py

---
+++ HelsinkiPython/Part4/everything_revrsed.py
@@ -0,0 +1,13 @@
+'''
+Please write a function named everything_reversed, which takes a list of strings as its argument. 
+The function returns a new list with all of the items on the original list reversed. 
+Also the order of items should be reversed on the new list.
+
+An example of how the function should work:
+
+my_list = ["Hi", "there", "example", "one more"]
+new_list = everything_reversed(my_list)
+print(new_list)
+Sample output
+['erom eno', 'elpmaxe', 'ereht', 'iH']
+'''
